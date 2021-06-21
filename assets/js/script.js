c2bd8d1286de08a74dce619a004aef34f46e7b31
KevinFromConn <kfc21194@gmail.com>
2021-06-20 22:07:47 -0400

    got clock to work as it should, still working on getting the game to end when it should

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,29 +16,29 @@ var choice4= document.querySelector("#choice4")
       { 
         q: 'How would you write developer comments in JavaScript?', 
         c: ['<!-- --!>','// ','/* */','none of the above'],
-        a: 1 
+        a: '// '
       
       },
       { 
         q: 'Which of the following is an example of a string?', 
         c: ['14','1.4','1e4','One and Four'],
-        a: 3 
+        a: 'One and Four'
       
       },
       {
         q: 'How would you contain multiple items in an array?',
         c: ['[]', '{}', '<>', '()'],
-        a: 0
+        a: '[]'
       },
       {
         q: 'How would you convert items in an array from a string to a number?',
         c: ['JSON.stringify()', '.toNumber', 'JSON.parseInt()', 'document.querySelector()'],
-        a: 2
+        a: 'JSONparseInt()'
       },
       {
         q: 'Where in the HTML document should you write or link your JavaScript?',
         c: ['In the <head> tag', 'In a <script> tag', 'Anywhere in the <body> tag', 'Any of the above'],
-        a: 1
+        a: 'In a <script> tag'
       }
       
     ];
@@ -68,33 +68,93 @@ var choice4= document.querySelector("#choice4")
     }
 
   choice1.addEventListener("click", function() {
-    questionIndex++
-    loadQuestion()
+    
+    console.log(questions[questionIndex].a)
+    if (choice1.textContent === questions[questionIndex].a) {
+      questionIndex++;
+      if (questionIndex === questions.length) {
+        endGame()
+      } else {
+        loadQuestion()
+      }
+    }
+    else {questionIndex++;
+      timeRemaining -= 15; 
+      loadQuestion()}
     })
   choice2.addEventListener("click", function() {
-    questionIndex++
-    loadQuestion()
+    
+    if (choice2.textContent === questions[questionIndex].a) {
+      questionIndex++;
+      if (questionIndex === questions.length) {
+        endGame()
+      } else {
+        loadQuestion()
+      }
+    }
+    else {questionIndex++;
+       timeRemaining -= 15;
+        loadQuestion()}
     })
   choice3.addEventListener("click", function() {
-    questionIndex++
-    loadQuestion()
+   
+    if (choice3.textContent === questions[questionIndex].a) {
+      questionIndex++
+      if (questionIndex === questions.length) {
+        endGame()
+      } else {
+        loadQuestion()
+      }
+    }
+    else {questionIndex++; 
+      timeRemaining -= 15; 
+      loadQuestion()}
     })
   choice4.addEventListener("click", function() {
-    questionIndex++
-    loadQuestion()
+    
+    if (choice4.textContent === questions[questionIndex].a) {
+      questionIndex++
+      if (questionIndex === questions.length) {
+        endGame()
+      } else {
+        loadQuestion()
+      }
+    }
+    else {questionIndex++; 
+      timeRemaining -= 15; 
+      loadQuestion()}
     })
   
     
     function countDown(){
-      if(timeRemaining===0)
+      if(timeRemaining === 0)
       {
         clearInterval(pidClock)  
         questionsDiv.classList.add("hide")  //adding the hide class to make it invisible
         initialDiv.classList.remove("hide") // removing the hide class to make it visible
       }
+      endGame()
       
       TimeLeftSpan.textContent = timeRemaining
-      timeRemaining--  // timeRemaining = timeRemaining-1
-
-
+      timeRemaining = timeRemaining-1
     }
+
+   function endGame() {
+      // create a div and have it display "your score is" + timeRemaining
+      clearInterval();
+      var endGameDiv = document.createElement("div")
+        endGameDiv.innerText = "Your Score Is" + timeRemaining
+
+      var highScoresForm = document.createElement("form")
+        highScoresForm.setAttribute("id", "myForm")
+      var highScoresInput = document.createElement("input")
+        highScoresInput.setAttribute("type", "text")
+       // highScoresInput.appendChild("myForm")
+      var highScoresSubmit = document.createElement("button")
+        highScoresSubmit.addEventListener("click", function() {
+          document.location.href = "./highscore.html"
+
+        })
+      // clearInterval
+      // create a form where you submit your initials
+   }
